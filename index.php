24ce607e701b19e8b5124d819acf62f46533e92d
Hendry <ruiqingzheng@hotmail.com>
2019-06-28 14:04:17 +0800

    初始化项目， 初始化页面结构

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.bootcss.com/materialize/1.0.0-rc.2/css/materialize.css" rel="stylesheet">
+    <title>Products List page</title>
+</head>
+<body class="grey lighten-4">
+<nav>
+    <div class="nav-wrapper teal">
+        <a href="#" class="brand-logo">Product List demo -- php</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="#">Link1</a></li>
+            <li><a href="#">Link2</a></li>
+            <li><a href="#">Link3</a></li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
+
+
+
+</div>
+
+
+<footer class="section">
+    <div class="center grey-text">&copy; Copyright 2019</div>
+</footer>
+
+</body>
+</html>
