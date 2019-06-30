6f34680bf0e70053be7cf2eb94c56cd5bcbff8b5
Hendry <ruiqingzheng@hotmail.com>
2019-06-30 15:38:31 +0800

    api跨域头信息修改

--- api/product/getall.php
+++ api/product/getall.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 
 require __DIR__ . '/../../model/Products.php';
 $conn = require(__DIR__ . '/../../db/db_connect.php');
